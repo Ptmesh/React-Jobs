6b20e0523a663015455fc21d824091be20068867
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2024-03-22 11:27:20 +0530

    Done with the component structure and json for home page

--- reactjobs/src/App.jsx
+++ reactjobs/src/App.jsx
@@ -1,8 +1,19 @@
+import Hero from "./components/Hero.jsx";
+import HomeCards from "./components/HomeCards.jsx";
+import JobListing from "./components/JobListing.jsx";
 import Navbar from "./components/Navbar.jsx";
+import ViewAllJobs from "./components/ViewAllJobs.jsx";
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero
+        title="Become A React Dev"
+        subtitle="Find the React job that fits your skills and needs "
+      />
+      <HomeCards />
+      <JobListing />
+      <ViewAllJobs />
     </>
   );
 };
